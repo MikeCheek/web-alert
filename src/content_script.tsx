61d0f943fef1fb77ddb264b880ca3468e9b47b8f
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-10-13 07:54:02 +0200

    Some working edits

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,43 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
+let stopped = false;
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.reload && !stopped) {
+    sendResponse('Ok, I reload');
+    reloadAndCheck();
+  }
+  if (msg.restart) {
+    stopped = false;
+    sendResponse('Ok, I restart');
   }
 });
+
+// chrome.tabs.onUpdated.addListener((_tabId, info) => {
+//   setInterval(() => {
+//     console.log('2s Interval');
+//     if (info.status === 'complete') {
+//       if (document) {
+//         const bodyHtml = document.body.innerHTML;
+//         console.log(bodyHtml);
+//         const pattern = /.*Michele.*/gi;
+//         if (bodyHtml.match(new RegExp(pattern))) {
+//           stopped = true;
+//           console.log('MATCH');
+//         }
+//       }
+//     }
+//   }, 2000);
+// });
+
+const reloadAndCheck = () => {
+  window.location.reload();
+};
+
+// chrome.runtime.onConnect.addListener((port) => {
+//   if (port.name == 'channelName') {
+//     port.onMessage.addListener((response) => {
+//       if (response.reload) {
+//         window.location.reload();
+//       }
+//     });
+//   }
+// });
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,31 +1,64 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 
 const Popup = () => {
   const [count, setCount] = useState(0);
   const [currentURL, setCurrentURL] = useState<string>();
+  const [reload, setReload] = useState<boolean>(false);
+
+  let interval: NodeJS.Timer;
 
   useEffect(() => {
     chrome.action.setBadgeText({ text: count.toString() });
   }, [count]);
 
   useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       setCurrentURL(tabs[0].url);
     });
   }, []);
 
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  const toggleReload = () => {
+    if (reload) {
+      clearInterval(interval);
+      setReload(false);
+    } else {
+      setReload(true);
+      interval = setInterval(() => {
+        sendReload();
+        console.log('runned');
+      }, 10000);
+    }
+  };
+
+  const sendReload = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const tab = tabs[0];
       if (tab.id) {
         chrome.tabs.sendMessage(
           tab.id,
           {
-            color: "#555555",
+            reload: true,
           },
           (msg) => {
-            console.log("result message:", msg);
+            console.log('result message:', msg);
+          }
+        );
+      }
+    });
+  };
+
+  const sendRestart = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tab = tabs[0];
+      if (tab.id && reload) {
+        chrome.tabs.sendMessage(
+          tab.id,
+          {
+            restart: true,
+          },
+          (msg) => {
+            console.log('result message:', msg);
           }
         );
       }
@@ -34,17 +67,21 @@ const Popup = () => {
 
   return (
     <>
-      <ul style={{ minWidth: "700px" }}>
+      <ul style={{ minWidth: '700px' }}>
         <li>Current URL: {currentURL}</li>
         <li>Current Time: {new Date().toLocaleTimeString()}</li>
       </ul>
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{ marginRight: "5px" }}
-      >
+      <button onClick={() => setCount(count + 1)} style={{ marginRight: '5px' }}>
         count up
       </button>
-      <button onClick={changeBackground}>change background</button>
+      {reload ? (
+        <p>Reloading</p>
+      ) : (
+        <button onClick={toggleReload} style={{ marginRight: '5px' }}>
+          Start reload
+        </button>
+      )}
+      <button onClick={sendRestart}>Send restart</button>
     </>
   );
 };
@@ -53,5 +90,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Popup />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
