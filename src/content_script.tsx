b71809bf6408d816d8eba043dc4cb54893f1b4bc
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-10-15 20:17:48 +0200

    Fixed and improved things

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -16,7 +16,7 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   }
   if (msg.get === 'body') {
     if (document) {
-      window.scrollTo(0, 5000);
+      // window.scrollTo(0, 5000);
       checkInNodes(document.body.getElementsByTagName('*'));
       if (!stopped) console.log("I'll continue reloading");
     }
@@ -29,8 +29,24 @@ const reload = () => {
   window.location.reload();
 };
 
+const itemsToRemove = [
+  'xzkaem6 x13vifvy xixxii4 x1e558r4 x1pi30zi xds687c',
+  'x78zum5 x1q0g3np xl56j7k x1yztbdb x1y1aw1k',
+  'x78zum5 x1q0g3np xl56j7k x1yztbdb x1y1aw1k',
+  'x1yztbdb',
+  'x9f619 x1ja2u2z xnp8db0 x112wk31 xnjgh8c xxc7z9f x1t2pt76 x1u2d2a2 x6ikm8r x10wlt62 x1xzczws x7wzq59 xxzkxad x9e5oc1',
+  'x9f619 x1ja2u2z xnp8db0 x112wk31 xnjgh8c xxc7z9f x1t2pt76 x1u2d2a2 x6ikm8r x10wlt62 x7wzq59 xxzkxad x1daaz14',
+  'xixxii4 x1ey2m1c xds687c',
+];
+
+const removeElementByClassName = (className: string) => {
+  const toRemove = document.getElementsByClassName(className)[0];
+  if (toRemove) toRemove.remove();
+};
+
 const checkInNodes = (collection: HTMLCollectionOf<Element>) => {
   found = false;
+  itemsToRemove.forEach((item) => removeElementByClassName(item));
   Array.from(collection)
     .filter(
       (element) =>
@@ -56,27 +72,33 @@ const checkInNodes = (collection: HTMLCollectionOf<Element>) => {
 
             if (found) {
               stopped = true;
-              (element as HTMLElement).style.backgroundColor = 'yellow';
-              (element as HTMLElement).style.border = 'solid red 5px';
-              (element as HTMLElement).style.borderRadius = '10px';
-              window.scrollTo(0, element.getBoundingClientRect().top);
               console.log('found ' + word + ', I stopped reloading\n Click restart to continue searching');
               console.log(element);
-              sendNotification(word);
+              focusElement(element as HTMLElement);
+              sendNotification(element.textContent ?? word, () => focusElement(element as HTMLElement));
             }
           }
         });
     });
 };
 
-const sendNotification = (word: string) => {
+const focusElement = (element: HTMLElement) => {
+  element.style.backgroundColor = 'yellow';
+  element.style.border = 'solid red 5px';
+  element.style.borderRadius = '10px';
+  window.focus();
+  window.scrollTo(0, element.getBoundingClientRect().top - 150);
+};
+
+const sendNotification = (word: string, action: () => void) => {
   const notification = 'Found word "' + word + '"';
   if ('Notification' in window) {
     if (window.Notification.permission != 'granted')
       window.Notification.requestPermission().then((permission) => {
-        if (permission === 'granted') new window.Notification(notification);
+        if (permission === 'granted') new window.Notification(notification).addEventListener('click', () => action());
       });
-    else if (window.Notification.permission === 'granted') new window.Notification(notification);
+    else if (window.Notification.permission === 'granted')
+      new window.Notification(notification).addEventListener('click', () => action());
   }
 };
 
